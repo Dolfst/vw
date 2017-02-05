c665b0827a9007c361d75755c0a0d540b4064394
Дмитрий <Dolfst@gmail.com>
2017-02-05 22:43:25 +0200

    4

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'golf-2',
+		wp_ws_url : 'http://dolfst.bget.ru/wp-appkit-api/golf-2',
+		wp_url : 'http://dolfst.bget.ru',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_title : 'Golf Sylvester',
+		app_platform : 'android',
+		gmt_offset : 3,
+		debug_mode : 'off',
+		auth_key : 'DvepYMYSo4vSJ;W$1tAE)B`pDer%@@7sFI#?9lO/KMWBh9vWJpbr459}+9J1g#oM',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
